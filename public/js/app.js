85256aaa060d0c6ce6502c46321bdd61e9e62ec7
Michael Murvine <michael.murvine@gmail.com>
2019-10-25 18:21:10 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    });
-});
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     });
+// });
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
@@ -17,7 +17,7 @@ weatherForm.addEventListener('submit', (e) => {
     message1.textContent = 'Loading...';
     message2.textContent = '';
 
-    fetch('http://localhost:3000/weather?address=' + encodeURIComponent(location)).then((response) => {
+    fetch('/weather?address=' + encodeURIComponent(location)).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 // console.log(data.error);
