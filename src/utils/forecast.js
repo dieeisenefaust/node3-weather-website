9eb2390bc406c3b4d0956f5a50ea189f9faed913
Michael Murvine <michael.murvine@gmail.com>
2019-10-27 14:02:40 -0500

    Added additional forecast information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find the specified location.', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain and wind speeds of ' + body.currently.windSpeed + ' mph.');
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out with a low of ' + body.daily.data[0].temperatureLow + ' and high of ' + body.daily.data[0].temperatureHigh + '. There is a ' + body.currently.precipProbability + '% chance of rain. Wind speeds of up to ' + body.currently.windSpeed + ' mph.');
         }
     });
 };
